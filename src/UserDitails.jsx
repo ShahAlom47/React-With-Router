e2c067e4bb426940a08f54626d5d6ab31932ab69
sah alom <sahalom4729@gmail.com>
2024-03-20 15:24:23 +0400

    add useNavigate for back btn

--- src/UserDitails.jsx
+++ src/UserDitails.jsx
@@ -1,4 +1,4 @@
-import { useLoaderData } from "react-router-dom";
+import { useLoaderData, useNavigate } from "react-router-dom";
 
 
 
@@ -6,12 +6,21 @@ import { useLoaderData } from "react-router-dom";
 const UserDetails = () => {
     const userDitails = useLoaderData();
     const {street,city,zipcode}=userDitails.address
+    const navigate =useNavigate()
+
+const backhndel =()=>{
+
+navigate(-1)
+
+}
+
     return (
         <div className="text-center p-4 bg-slate-400">
             <h1>User details</h1>
             <p>Street : {street}</p>
             <p>City : {city}</p>
             <p>Zip Code : {zipcode}</p>
+            <button onClick={()=>backhndel()} className="btn btn-ghost" > Go Back</button>
         </div>
     );
 };
